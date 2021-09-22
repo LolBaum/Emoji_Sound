3446445fc0f356ba822604081bc92e03317c4d33
louziferr <luzie.ahrens@gmail.com>
2021-09-22 15:21:34 +0200

    worked over comments

--- Python/EmojiSound.py
+++ Python/EmojiSound.py
@@ -5,14 +5,20 @@ from oscpy.client import OSCClient
 import json
 
 
-##### class to get emoji-sentiment score from a dict and send and OSC-Message #####
+##### Class to get Emoji-sentiment scores for a given emoji and send an OSC-Message.             #########
+##### The emoji-sentiment scores are supplied by a json-file (emoji_scores_new.json).            #########
 
+##### OSC-Message Format:                                                                       ##########
+##### -keyword -overall_score -negative_score -neutral_score -positive_score -azimuth -elevation  ########
+
+##### The ip has to be the one that SuperCollider is running on.                                 #########
+##### The port has to be the one that SuperCollider is receiving OSC-Messages on (default 57120). ########
+##### The emoji_score_file has to be supplied in the right format (see emoji_scores_new.json).   #########
 class EmojiSound:
     def __init__(self,
                  ip="127.0.0.1",
                  port=57120,
                  emoji_score_file="emoji_scores_new.json"):
-        # self.analyzer = SentimentIntensityAnalyzer()
         self.sc_server_ip = ip
         self.sc_port = port
         self.osc = OSCClient(ip, port, encoding="utf-8")
@@ -24,28 +30,26 @@ class EmojiSound:
             return self.emoji_scores[emoji]["sentsc"]
         return 0
 
-    @staticmethod
-    def synth_freq(sentsc):
-        return 300+(200*sentsc)
-
-    # function to get random tweet from ItemIterator of Tweets
+    # function to get the "negative score" for a given emoji
     def emoji_neg(self, emoji):
         if emoji in self.emoji_scores:
             return self.emoji_scores[emoji]["neg"]
         return 0
 
-    # function to get random tweet from ItemIterator of Tweets
+    # function to get the "neutral score" for a given emoji
     def emoji_neut(self, emoji):
         if emoji in self.emoji_scores:
             return self.emoji_scores[emoji]["neut"]
         return 0
 
-    # function to get random tweet from ItemIterator of Tweets
+    # function to get the "positive score" for a given emoji
     def emoji_pos(self, emoji):
         if emoji in self.emoji_scores:
             return self.emoji_scores[emoji]["pos"]
         return 0
 
+
+    # function to get all sentiment scores for an emoji and save them in a dict
     def get_sentiment_stats(self, emoji):
         msg_dict = dict()
         msg_dict["emoji"] = emoji
@@ -55,9 +59,9 @@ class EmojiSound:
         msg_dict["pos"] = float(self.emoji_pos(emoji))  # sentiment ranges from -1 to 1
         return msg_dict
     
-    # OSC Message that is send to the server
-    # contains: synth, frequency-range, tempo, panning
-    # frequency range: 50-820
+    # function to send OSC-Message in the following format:
+    # -keyword -overall_score -negative_score -neutral_score -positive_score -azimuth -elevation
+
     def send_osc_msg(self, emoji, azim=0.4, elev=0.1, print_info=False):
         osc_dict = self.get_sentiment_stats(emoji)
         msg = list()
@@ -71,7 +75,7 @@ class EmojiSound:
         if print_info:
             self.print_info(osc_dict)
 
-    # function to print info from tweet
+    # function to print sentiment-info from emoji
     def print_info(self, osc_dict):
         print("Emoji: " + str(osc_dict["emoji"]))
         print("Negativ: " + str(osc_dict["neg"]))
@@ -79,12 +83,13 @@ class EmojiSound:
         print("Positiv: " + str(osc_dict["pos"]))
         print("Sentiment: " + str(osc_dict["sentsc"]))
 
-
+# FOR TESTING
 if __name__ == "__main__":
+    # create object
     EmSo = EmojiSound()
-
+    # receive emoji via terminal-input
     emoji = input("Enter emoji: ")
     while emoji != "":
+        # send OSC-Message
         EmSo.send_osc_msg(emoji)
         emoji = input("Enter emoji: ")
-    # EmSo.send_osc_message("hey")
